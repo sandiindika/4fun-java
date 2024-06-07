95013b81eed33ad3244ea2a611b488bfd6b4acd2
Sandi Indika Saputra <sandidikaputra@gmail.com>
2024-06-08 01:24:11 +0700

    ~

--- enigma-university-oop/src/com/enigma/Main.java
+++
@@ -1,8 +0,0 @@
-package com.enigma;
-
-public class Main {
-    public static void main(String[] args) {
-        Menu menu = new Menu();
-        menu.start();
-    }
-}
--- enigma-university-oop/src/com/enigma/services/StudentService.java
+++
@@ -1,7 +0,0 @@
-package com.enigma.services;
-
-public interface StudentService {
-    void addStudent();
-    void viewStudent();
-    void deleteStudent();
-}
